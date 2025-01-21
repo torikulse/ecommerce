3c48544d34915be1ee1cdbebc273845f77775f0e
Torikul <devtorikul@gmail.com>
2025-01-21 23:10:57 +0600

    image lazy loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card(props) {
         <div className="absolute rounded bg-black p-2 text-white">
           {props.discountPercentage} %
         </div>
-        <img src={props.thumb} className="w-full" alt="" />
+        <img loading="lazy" src={props.thumb} className="w-full" alt="" />
         <div className="absolute -bottom-28 w-full transition-all duration-300 group-hover:bottom-0">
           <Overlay />
         </div>
@@ -15,7 +15,7 @@ function Card(props) {
       <div className="flex justify-between pt-2">
         <h1 className="text-2xl font-semibold text-neutral-800">
           {props.title}
-        </h1>{" "}
+        </h1>
         <h2 className="min-w-fit text-xl font-semibold text-orange-600">
           ${props.price}
         </h2>
